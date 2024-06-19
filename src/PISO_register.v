a02e7d7de853c38d675c48a56cfb8ced7f59a601
Coline3003 <116337158+Coline3003@users.noreply.github.com>
2024-06-19 14:35:37 +0200

    Update PISO_register.v

--- src/PISO_register.v
+++ src/PISO_register.v
@@ -13,14 +13,14 @@ always @(posedge clk) begin
 	
 	register[11:0] = parallel_in[11:0];
 	serial_out <= register[0];
-	register <= register >>> 1;
+	register = register >>> 1;
 
 
 	       
     end
     else if (SL==0) begin
 	serial_out <= register[0];
-        register <= register >>> 1;
+        register = register >>> 1;
 	 // Output the least significant bit
     end
   end
