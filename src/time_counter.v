77984ba98e1ace9e54b395dcad282cb9deecdc92
Coline3003 <116337158+Coline3003@users.noreply.github.com>
2024-06-19 12:15:26 +0200

    Update time_counter.v

--- src/time_counter.v
+++ src/time_counter.v
@@ -8,17 +8,30 @@ initial begin
   min = 6'b0;
 end
 
+wire clear_ovf;
+assign clear_ovf = rst_ovf || reset;
+
+always @(posedge clk or posedge clear_ovf) begin
+
+        if(clear_ovf == 1) begin
+		ovf <= 0;
+    	end
+	else begin
+		if(sec == 59 & min == 59) begin
+			ovf <= 1;
+		end
+	end
+end
+	
+
 //-- Sensitive to rising edge
   always @(posedge clk or posedge reset) begin
   //-- Incrementar el registro
     if(reset == 1) begin
 	sec <= 6'b0;
 	min <= 6'b0;
-	ovf <= 0;
     end
-    else if(rst_ovf == 1) begin
-	ovf <= 0;
-    end
+
     else begin
     sec <= sec + 1;
     if(sec == 59) begin
@@ -28,7 +41,7 @@ end
     if(sec == 59 & min == 59) begin
       sec <= 6'b0;
   	min <= 6'b0;
-      ovf <= 1;
+      
     end
   end
   end
