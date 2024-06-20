caf718b38237ed334b9c23c52c07a3d28b34fddb
Coline3003 <116337158+Coline3003@users.noreply.github.com>
2024-06-20 11:48:40 +0200

    Update PISO_register.v

--- src/PISO_register.v
+++ src/PISO_register.v
@@ -6,21 +6,18 @@ module PISO_register(
   output reg [11:0] register
 );
   
-  //reg [12:0] register;
 
 always @(posedge clk) begin
     if(SL==1) begin
 	
-	    register[11:0] = parallel_in[11:0];
-		serial_out = register[0];
-		register = register >>> 1;
-
-
+            register[10:0] <= parallel_in[11:1];
+	    serial_out <= parallel_in[0];
 	       
     end
     else if (SL==0) begin
-	serial_out = register[0];
-        register = register >>> 1;
+	
+	serial_out <= register[0];
+        register <= register >>> 1;
 	 // Output the least significant bit
     end
   end
