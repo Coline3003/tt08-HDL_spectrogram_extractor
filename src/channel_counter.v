19e9949c216c2dee9175190ae6153520dfc03d5d
Coline3003 <116337158+Coline3003@users.noreply.github.com>
2024-06-21 09:27:51 +0200

    Update channel_counter.v

--- src/channel_counter.v
+++ src/channel_counter.v
@@ -1,11 +1,18 @@
+/*
+purpose : count each emission when detect a rising edge
+impulse : signal coming from analog design (resonance of one of the piezo sensors) 
+reset : when ('1') reset the counter
+enable : when ('0') allows the counter to increment
+data : value of the counter (from 0 to 4095)
+ovf : '1' when the counter overflows
+*/
+
 module channel_counter(input impulse, input reset, input enable, output reg [11:0] data, output reg ovf
                        );
 
-    
 
-//-- Sensitive to rising edge
   always @(posedge reset or posedge impulse) begin
-  //-- Incrementar el registro
+	  
     if(reset == 1) begin
 	data <= 12'b0;
 	ovf <= 0;	
