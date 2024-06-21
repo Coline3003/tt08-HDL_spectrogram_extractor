0c4dc2766a5b8635ce5ca131738d4c1a90fd99fc
Coline3003 <116337158+Coline3003@users.noreply.github.com>
2024-06-21 09:21:25 +0200

    Update PISO_register.v

--- src/PISO_register.v
+++ src/PISO_register.v
@@ -1,24 +1,32 @@
+/*
+clk : clock for shif and load operations
+parallel_in : output of the MUX, data to be send
+SL : '1' => load, '0' => shift
+serial_out : data serialization
+*/
+
 module PISO_register(
   input wire clk,
   input wire [11:0] parallel_in,
   input wire SL, 
-  output reg serial_out,
-  output reg [11:0] register
+  output reg serial_out
+  
 );
   
-
+reg [10:0] register;
+	
 always @(posedge clk) begin
     if(SL==1) begin
 	
-            register[10:0] <= parallel_in[11:1];
-	    serial_out <= parallel_in[0];
+	    register[10:0] <= parallel_in[11:1]; // Load 
+	    serial_out <= parallel_in[0];  // Output the least significant bit
 	       
     end
     else if (SL==0) begin
 	
-	serial_out <= register[0];
-        register <= register >>> 1;
-	 // Output the least significant bit
+	serial_out <= register[0]; // Output the least significant bit
+        register <= register >>> 1; //shift
+	
     end
   end
 
